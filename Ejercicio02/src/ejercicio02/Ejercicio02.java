c2925bb7d9da2405ab55e04866f1adf9ab1100f2
Jonathan Coronel <jacoronel10@utpl.edu.ec>
2021-10-29 17:04:56 -0500

    primer commit

---
+++ Ejercicio02/src/ejercicio02/Ejercicio02.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio02;
+
+/**
+ *
+ * @author JonathanCoronel
+ */
+public class Ejercicio02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Resolucion del problema
+        int variable1 = 10;
+        int variable2 = 2;
+        System.out.println("-------Solución a problema------------------");
+        double resultado = Math.pow(variable1, variable2) + variable1;
+        System.out.println(resultado);
+    }
+
+}
---
+++ Ejercicio09/src/ejercicio09/Ejercicio09.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio09;
+
+/**
+ *
+ * @author JonathanCoronel
+ */
+public class Ejercicio09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Resolucion del ejercicio09
+        int var1 = 81;
+        int var2 = 25;
+        int var3 = 10;
+        int var4 = 50;
+
+        boolean resultado = Math.sqrt(81) + Math.sqrt(var2) * var3 >= var4;
+        System.out.println(resultado);
+    }
+
+}
